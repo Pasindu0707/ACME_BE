41553177c8df262629e17f863ea6f5a8e202a1ed
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2025-02-26 23:39:39 +0530

    commit

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],  // Allow multiple origins
+  origin: ['https://acme-fe-three.vercel.app','http://localhost:5173', 'http://localhost:3000'],  // Allow multiple origins
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 };
